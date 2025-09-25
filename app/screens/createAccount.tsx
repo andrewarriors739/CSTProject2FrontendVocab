906da85ebb9c199aa68e66cbc91ef8ceaf7e4269
Andre Gutierrez <andregutierrez03@gmail.com>
2025-09-25 13:27:20 -0700

    testing

--- app/screens/createAccount.tsx
+++ app/screens/createAccount.tsx
@@ -1,3 +1,5 @@
+//Andre G
+
 import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, Button, StyleSheet, Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
